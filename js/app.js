824e7bb3617021eb35106eac7319563d55b02ba4
Wesley Anjos <wesleyanjosoficial@gmail.com>
2026-05-06 19:18:25 -0300

    update - aula 06 bom p2

--- js/app.js
+++ js/app.js
@@ -2,5 +2,14 @@ console.log(
     screen.width,
     screen.height,
     screen.orientation,
-    screen.orientation.type
+    screen.orientation.type,
+    navigator,
+    navigator.appName
+)
+
+console.log(
+    navigator.appVersion,
+    location,
+    // location.href = 'http://www.google.com/'
+    history
 )
